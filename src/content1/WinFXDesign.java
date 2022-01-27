cfc4fc3980938127d30c1ca763d423527692aa01
JingxinJiang <jiajingx@sheridancollege.ca>
2022-01-27 00:28:13 -0500

    first come part1

---
+++ src/content1/WinFXDesign.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package content1;
+
+/**
+ *
+ * @author seanz
+ */
+public class WinFXDesign {
+    private int name;
+
+    public int getName() {
+        return name;
+    }
+    
+    
+}
